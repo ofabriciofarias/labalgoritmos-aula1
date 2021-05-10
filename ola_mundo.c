94936f6cc508dbefb5df924ff2650e181bd1729c
ofabriciofarias <fabriciosf@ufpa.br>
2021-05-10 14:55:06 -0300

    Create ola_mundo.c

---
+++ ola_mundo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){ //Inicio do Programa
+
+//Imprimir uma mensagem na tela para o usuario
+printf("Ola Mundo!"); //Aparecera a mensagem ola mundo. O \n significa uma quebra de linha.
+
+
+
+} //Fim do Programa
